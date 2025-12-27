e33eed47aa0340f251c9f33c82f226bce070b8bd
Rishi <rishikm215@gmail.com>
2025-12-27 11:09:49 +0530

    binary string without consecutive 1

---
+++ recursion/binaryString.java
@@ -0,0 +1,24 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class binaryString {
+    public static void printBinString(int n, int lp, String str) {
+        if (n == 0) {
+            System.out.println(str);
+            return;
+        }
+        printBinString(n - 1, 0, str + "0");
+        if (lp == 0) {
+            printBinString(n - 1, 1, str + "1");
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size (n) :");
+        int n = sc.nextInt();
+        printBinString(n, 0, "");
+
+    }
+}
